2ca5feab05de16fba34791e6e5f6005dfef3360e
Stephen Thornquist <thornquist.stephen@gmail.com>
2024-07-01 19:33:51 -0400

    CI builds but does not run tests or benchmarks yet...

--- benches/opening_files_benchmark.rs
+++ benches/opening_files_benchmark.rs
@@ -1,21 +1,21 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use corrosiff::{self, SiffReader};
+use corrosiff::{self, SiffReader, CorrosiffError};
 
 const MOUNT_SIFF_PATH :&str = "/Users/stephen/maimondata01/Stephen/Imaging/2024-04/2024-04-23/R70B05_GFlamp2/Fly1/BarOnAtTen_1.siff";
 
-fn open_short_siff_test()->Result<SiffReader, std::io::Error>{
+fn open_short_siff_test()->Result<SiffReader, CorrosiffError>{
     corrosiff::open_siff(
         "/Users/stephen/Desktop/Data/imaging/2024-04/2024-04-07/Dh31_LexA_LKir_LGFlamp1/Fly1/BarOnAtTen_1.siff"
     )
 }
 
-fn open_long_siff_test()->Result<SiffReader, std::io::Error>{
+fn open_long_siff_test()->Result<SiffReader, CorrosiffError>{
     corrosiff::open_siff(
         "/Users/stephen/Desktop/Data/imaging/2024-04/2024-04-17/21Dhh_GCaFLITS/Fly1/Flashes_1.siff"
     )
 }
 
-fn open_mount_siff()->Result<SiffReader, std::io::Error>{
+fn open_mount_siff()->Result<SiffReader, CorrosiffError>{
     corrosiff::open_siff(MOUNT_SIFF_PATH)
 }
 
